431d943d227151da25c1b42157ab878e3bceefe2
David Saah <dasorange.hope@gmail.com>
2021-07-21 16:14:16 +0000

    rename git origin

---
+++ python/renamegit.py
@@ -0,0 +1,21 @@
+import os
+
+os.system("git remote -v")
+print("\n")
+
+cls = input("Do you want to clear old origin(Y/n): ").upper()
+repo = input("Enter name of repo: ")
+branch = input("Enter branch name: ")
+
+def clear(cls):
+    if cls == "" or cls == 'Y':
+        os.system("git remote remove origin")
+
+def rename(repo, branch):
+    os.system(f"git remote add origin git@github.com:DaveSaah/{repo}.git && git push --set-upstream origin {branch}")
+
+
+clear(cls)
+rename(repo, branch)
+print("\n")
+os.system("git remote -v")
