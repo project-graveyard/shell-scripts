e25932226e5b819b773b651dc3313eb83e4fd396
David Saah <dasorange.hope@gmail.com>
2022-07-17 01:24:04 +0000

    add pluto run script

---
+++ julia/start-pluto.jl
@@ -0,0 +1,10 @@
+import Pluto
+
+
+Pluto.run(
+          host="0.0.0.0", 
+	  port=8899,
+          require_secret_for_access=false,
+	  require_secret_for_open_links=false,
+	  launch_browser=false,
+	 )
